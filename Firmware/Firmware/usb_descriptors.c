b1d5ee0946a721ca3c889b6a414de170a24f5654
Pandaru <brault.lucas@orange.fr>
2026-02-28 22:55:47 +0100

    Updating usb_descriptors.c

--- Firmware/Firmware/usb_descriptors.c
+++ Firmware/Firmware/usb_descriptors.c
@@ -1,16 +1,133 @@
 #include "tusb.h"
 
+// --------------------------------------------------------------------
+// HID Report Descriptor
+// --------------------------------------------------------------------
+
 uint8_t const desc_hid_report[] =
 {
     TUD_HID_REPORT_DESC_KEYBOARD()
 };
 
-uint8_t const *tud_hid_descriptor_report_cb(void)
+uint8_t const * tud_hid_descriptor_report_cb(uint8_t instance)
 {
     return desc_hid_report;
 }
 
-uint16_t tud_hid_descriptor_report_size_cb(void)
+// --------------------------------------------------------------------
+// Device Descriptor
+// --------------------------------------------------------------------
+
+tusb_desc_device_t const desc_device =
 {
-    return sizeof(desc_hid_report);
+    .bLength            = sizeof(tusb_desc_device_t),
+    .bDescriptorType    = TUSB_DESC_DEVICE,
+    .bcdUSB             = 0x0200,
+
+    .bDeviceClass       = 0x00,
+    .bDeviceSubClass    = 0x00,
+    .bDeviceProtocol    = 0x00,
+
+    .bMaxPacketSize0    = CFG_TUD_ENDPOINT0_SIZE,
+
+    .idVendor           = 0xCAFE,
+    .idProduct          = 0xD00D,
+    .bcdDevice          = 0x0100,
+
+    .iManufacturer      = 0x01,
+    .iProduct           = 0x02,
+    .iSerialNumber      = 0x03,
+
+    .bNumConfigurations = 0x01
+};
+
+uint8_t const * tud_descriptor_device_cb(void)
+{
+    return (uint8_t const *) &desc_device;
+}
+
+// --------------------------------------------------------------------
+// Descriptor Configuration
+// --------------------------------------------------------------------
+
+enum
+{
+    ITF_NUM_HID,
+    ITF_NUM_TOTAL
+};
+
+#define CONFIG_TOTAL_LEN  (TUD_CONFIG_DESC_LEN + TUD_HID_DESC_LEN)
+
+uint8_t const desc_configuration[] =
+{
+
+    TUD_CONFIG_DESCRIPTOR(1, ITF_NUM_TOTAL, 0, CONFIG_TOTAL_LEN, 0x00, 100),
+
+
+    TUD_HID_DESCRIPTOR(ITF_NUM_HID, 0, HID_ITF_PROTOCOL_KEYBOARD,
+                       sizeof(desc_hid_report), 0x81, 8, 10)
+};
+
+uint8_t const * tud_descriptor_configuration_cb(uint8_t index)
+{
+    return desc_configuration;
+}
+
+// --------------------------------------------------------------------
+// String Descriptors
+// --------------------------------------------------------------------
+
+char const* string_desc_arr[] =
+{
+    (const char[]) { 0x09, 0x04 },
+    "Pandaru",
+    "Red Ducky",
+    "RD-0001"
+};
+
+static uint16_t _desc_str[32];
+
+uint16_t const* tud_descriptor_string_cb(uint8_t index, uint16_t langid)
+{
+    uint8_t chr_count;
+
+    if ( index == 0 )
+    {
+        memcpy(&_desc_str[1], string_desc_arr[0], 2);
+        chr_count = 1;
+    }
+    else
+    {
+        const char* str = string_desc_arr[index];
+        chr_count = strlen(str);
+
+        for(uint8_t i=0; i<chr_count; i++)
+        {
+            _desc_str[1+i] = str[i];
+        }
+    }
+
+    _desc_str[0] = (TUSB_DESC_STRING << 8 ) | (2*chr_count + 2);
+
+    return _desc_str;
+}
+
+// --------------------------------------------------------------------
+// HID Callbacks
+// --------------------------------------------------------------------
+
+void tud_hid_set_report_cb(uint8_t instance,
+                           uint8_t report_id,
+                           hid_report_type_t report_type,
+                           uint8_t const* buffer,
+                           uint16_t bufsize)
+{}
+
+uint16_t tud_hid_get_report_cb(uint8_t instance,
+                               uint8_t report_id,
+                               hid_report_type_t report_type,
+                               uint8_t* buffer,
+                               uint16_t reqlen)
+{
+    return 0;
 }
