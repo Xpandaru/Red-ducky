febb1ca4d48f3f1e08d85d963a7cdb55bf04a020
Pandaru <brault.lucas@orange.fr>
2026-03-07 11:58:37 +0100

    Updating main.c

--- Firmware/Firmware/main.c
+++ Firmware/Firmware/main.c
@@ -4,6 +4,10 @@
 #include "tusb.h"
 #include "layout.h"
 #include "parser.h"
+#include "cdc_input.h"
+#include "script_storage.h"
+
+#define MAX_SCRIPT_LINES 200
 
 typedef enum {
     TYPE_IDLE,
@@ -11,11 +15,25 @@ typedef enum {
     TYPE_RELEASE
 } type_state_t;
 
+typedef enum {
+    MODE_ACTIVE,
+    MODE_EDIT,
+    MODE_WRITING,
+    MODE_ERROR
+} SystemMode;
+
 // Variables declaration
 
+static SystemMode system_mode = MODE_ACTIVE;
+static char cdc_line_buffer[1009];
+static int cdc_error = 0;
+static int show_line_count = 0;
+static int write_line_count = 0;
+
+static int script_length = 0;
 static bool script_done = false;
 
-static const int global_delay = 1000;
+static int global_delay = 0;
 
 static type_state_t type_state = TYPE_IDLE;
 static const char* current_text = NULL;
@@ -131,6 +149,10 @@ void execute_command(Command* cmd) {
             delay_until = make_timeout_time_ms(cmd->value);
             break;
 
+        case CMD_GBDELAY:
+            global_delay = cmd->value;
+            break;
+
         case CMD_MODIFIER:
             if (cmd->keycode != 0) {
                 forced_keycode = cmd->keycode;
@@ -160,41 +182,185 @@ int main() {
     board_init();
     tusb_init();
 
+    // LED variables
+
     const uint LED_PIN = 25;
 
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
+    absolute_time_t blink_delay = get_absolute_time();
+    bool led_state = false;
 
     while (!tud_hid_ready()) {
         tud_task();
     }
 
-    int current_line = 0;
-    static const char* const script[] = {
-        "WINDOWS r",
-        "STRING notepad",
-        "ENTER",
-        "REPEAT 5",
-        "STRINGLN test",
-        NULL
-    };
+    static char* script[MAX_SCRIPT_LINES];
+
+    if (script_is_present()) {
+        script_length = script_load(script, MAX_SCRIPT_LINES);
+    }
+    else system_mode = MODE_EDIT;
     
+    int current_line = 0;
+
     Command cmd;
     delay_until = get_absolute_time();
 
     while (true) {
         tud_task();
         hid_type_task();
-        gpio_put(LED_PIN, !script_done);
 
-        if (script[current_line] == NULL && typing_done) script_done = true;
+        switch (system_mode) {
+
+        case MODE_ACTIVE:
+            gpio_put(LED_PIN, !script_done);
+            led_state = !script_done;
+            break;
+        
+        case MODE_EDIT:
+        case MODE_WRITING:
+            if (time_reached(blink_delay)) {
+                led_state = !led_state;
+                gpio_put(LED_PIN, led_state);
+                blink_delay = make_timeout_time_ms(500);
+            }
+            break;
+
+        case MODE_ERROR:
+            if (time_reached(blink_delay)) {
+                led_state = !led_state;
+                gpio_put(LED_PIN, led_state);
+                if (led_state) blink_delay = make_timeout_time_ms(100);
+                else blink_delay = make_timeout_time_ms(200);
+            }
+            break;
+        
+        default:
+            break;
+        }
+
+        if (tud_cdc_available()) {
+            
+            if (cdc_read_line(cdc_line_buffer, sizeof(cdc_line_buffer), &cdc_error)) {
+
+                if (cdc_error == 1) {
+                    printf("ERROR(1): Buffer overflow : Your last entry exceeded the supported character limit.\n\n");
+                }
+
+                if (cdc_error == 2) {
+                    printf("ERROR(2): Buffer overflow : Your last entry exceeded the supported character limit.\n\n");
+                }
+
+                if (strcmp(cdc_line_buffer, "EDIT START") == 0) {
+
+                    printf("----------{EDIT MODE ENABLED}----------"
+                        "\nTo quit this mode, type : EDIT QUIT"
+                        "\n\nCommands :"
+                        "\n- SCRIPT EDIT : start editing the current script"
+                        "\n- SCRIPT BEGIN : remove the current script to create a new script, all commands after it are saved as the new script until SCRIPT END."
+                        "\n- SCRIPT END : marks the end of a script block and save it as the new script."
+                        "\n- SCRIPT DEL : delete the last line of the script."
+                        "\n- SCRIPT SHOW : show the current saved script."
+                        "\n\n"
+                    );
+                    system_mode = MODE_EDIT;
+                }
+
+                if (strcmp(cdc_line_buffer, "EDIT QUIT") == 0 && system_mode != MODE_WRITING) {
+
+                    printf("---------{EDIT MODE DISABLED}----------\n\n");
+                    system_mode = MODE_ACTIVE;
+                }
+
+                if (strcmp(cdc_line_buffer, "EDIT QUIT") == 0 && system_mode == MODE_WRITING) {
+                    printf("You cannot quit the EDIT MODE while editing the script. To quit the EDIT MODE, first type SCRIPT END.\n\n");
+                }
+
+                if (system_mode == MODE_EDIT) {
+
+                    if (strcmp(cdc_line_buffer, "SCRIPT BEGIN") == 0) {
+                        printf("Each new entry will be treated as a new line of the script. To exit this mode, type SCRIPT END.\n\n");
+                        script[0] = NULL;
+                        write_line_count = 0;
+                        system_mode = MODE_WRITING;
+                    }
+
+                    if (strcmp(cdc_line_buffer, "SCRIPT EDIT") == 0) {
+
+                        printf("---------------{SCRIPT}----------------\n");
+                        show_line_count = 0;
+                        while (script[show_line_count] != NULL) {
+                            printf("%s\n", script[show_line_count++]);
+                        }
+                        printf("\nType SCRIPT END to save the script.\n\n");
+
+                        write_line_count = script_length;
+                        system_mode = MODE_WRITING;
+                    }
+
+                    else if (strcmp(cdc_line_buffer, "SCRIPT END") == 0) {
+                        printf("You cannot finish a script that has not started. To start a script, type SCRIPT BEGIN.\n\n");
+                    }
+
+                    else if (strcmp(cdc_line_buffer, "SCRIPT DEL") == 0) {
+                        printf("This command is only accessible when editing or writing a script. To start editing a script, type SCRIPT EDIT, or to start a script, type SCRIPT EDIT.\n\n");
+                    }
+
+                    else if (strcmp(cdc_line_buffer, "SCRIPT SHOW") == 0) {
+                        printf("---------------{SCRIPT}----------------\n");
+
+                        show_line_count = 0;
+                        while (script[show_line_count] != NULL) {
+                            printf("%s\n", script[show_line_count++]);
+                        }
+                        printf("\n");
+                    }
+                }
+                else if (system_mode == MODE_WRITING) {
+
+                    if (strcmp(cdc_line_buffer, "SCRIPT END") == 0) {
+                        if (script_save(script)) printf("Script saved successfully !\n\n");
+                        else printf("ERROR(3) : Flash overflow : The script size exceeds the allocated space in the flash memory.\n\n");
+                        system_mode = MODE_EDIT;
+                    }
+
+                    else if (strcmp(cdc_line_buffer, "SCRIPT DEL") == 0) {
+                        write_line_count--;
+                        script[write_line_count] = NULL;
+
+                        printf("---------------{SCRIPT}----------------\n");
+                        show_line_count = 0;
+                        while (script[show_line_count] != NULL) {
+                            printf("%s\n", script[show_line_count++]);
+                        }
+                        printf("\nType SCRIPT END to save the script.\n\n");
+                    }
+
+                    else {
+                        script[write_line_count++] = strdup(cdc_line_buffer);
+                        script[write_line_count] = NULL;
+
+                        printf("---------------{SCRIPT}----------------\n");
+                        show_line_count = 0;
+                        while (script[show_line_count] != NULL) {
+                            printf("%s\n", script[show_line_count++]);
+                        }
+                        printf("\nType SCRIPT END to save the script.\n\n");
+                        
+                    }
+                }
+            }
+        }
+
+        if ((script[current_line] == NULL && typing_done) || system_mode != MODE_ACTIVE) script_done = true;
 
         if (typing_done && time_reached(delay_until) && script[current_line] != NULL && !script_done) {
             
             cmd = parse_line(script[current_line]);
             execute_command(&cmd);
 
-            if (cmd.type != CMD_DELAY && cmd.type != CMD_REPEAT && cmd.type != CMD_REM) delay_until = make_timeout_time_ms(global_delay);
+            if (cmd.type != CMD_DELAY && cmd.type != CMD_REPEAT && cmd.type != CMD_REM && cmd.type != CMD_GBDELAY) delay_until = make_timeout_time_ms(global_delay);
 
             if (repeat <= 0 || cmd.type == CMD_REPEAT) {
                 current_line++;
